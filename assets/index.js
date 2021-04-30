179304452b7b860014118c57b20429932e6de248
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-04-30 18:43:07 -0500

    updated index.js

--- assets/index.js
+++ assets/index.js
@@ -53,7 +53,7 @@ const start = () => {
                 "View All Departments", "Add Department", "Remove Department", "View Budget Allocated to Payroll"
             ]
     }).then((choice) => {
-        
+
         if (choice.menu === "View All Employees") allEmployees();
         if (choice.menu === "View Employees By Department") allEmpByDept();
         if (choice.menu === "View Employees By Role") allEmpByRole();
@@ -129,3 +129,201 @@ const empArr = (res) => {
     return employees;
 
 }
+
+let depArr = (res) => {
+    const departments = [];
+    res.map(obj => {
+        let newObj = {
+            name: obj.name,
+            value: {
+                id: obj.id,
+                name: obj.name
+            }
+        }
+        departments.push(newObj)
+    })
+    return departments;
+}
+
+const allEmployees = () => {
+    // this is where we show all the employees by querying the mySQL database
+    connection.query(`
+    SELECT 
+    e.id, e.first_name AS 'first name', e.last_name AS 'last name', r.title AS 'position', r.salary, CONCAT(m.first_name, " ", m.last_name) AS 'manager name'
+    FROM employee e
+    LEFT JOIN employee m
+    ON m.id = e.manager_id
+    INNER JOIN role r
+    ON r.id = e.role_id
+    JOIN department d
+    ON r.department_id = d.id;`, (err, res) => {
+        consoleResult(err, res);
+    });
+}
+
+const allEmpByDept = () => {
+    // this is where we sort employees by their departments
+
+    connection.query(`SELECT * FROM department;`, (err, res) => {
+        if (err) throw err;
+        //  const depts = res;
+        //  console.log(depts)
+
+        const departmentArr = depArr(res);
+        // consoleResult(err, res);
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Which department would you like to view?",
+            choices: departmentArr
+        }).then(answer => {
+            //  console.log(answer.department.name)
+            connection.query(`
+                SELECT e.id, e.first_name, e.last_name, d.name
+                FROM employee e
+                JOIN department d
+                JOIN role r
+                ON e.role_id = r.id AND r.department_id = d.id
+                WHERE d.name= ? ;
+            `, [answer.department.name], (err, res) => {
+                consoleResult(err, res)
+            })
+        }).catch((error) => {
+            console.log(error.message);
+            res.json({ error: error.message });
+        })
+    });
+}
+
+const allEmpByRole = () => {
+    // this is where we filter employees by their role
+    connection.query(`SELECT * FROM role;`, (err, res) => {
+        if (err) throw err;
+        // roleArr(res);
+        const rolesArr = roleArr(res);
+        consoleResult(err, res);
+        inquirer.prompt({
+            name: "role",
+            type: "list",
+            message: "Which role would you like to view?",
+            choices: rolesArr
+        }).then(answer => {
+            console.log(answer.role.title)
+            connection.query(`
+                SELECT e.id, e.first_name, e.last_name, r.title
+                FROM employee e
+                JOIN role r
+                ON e.role_id = r.id
+                WHERE r.title= ? ;
+            `, [answer.role.title], (err, res) => {
+                consoleResult(err, res)
+            })
+        }).catch((error) => {
+            console.log(error.message);
+            res.json({ error: error.message });
+        })
+    });
+}
+
+const allEmpByMan = () => {
+    // this is where we filter by manager
+    connection.query(`
+    SELECT DISTINCT m.id, m.first_name, m.last_name
+    FROM employee e
+    JOIN employee m
+    ON e.manager_id = m.id;
+    `, (err, res) => {
+        if (err) throw err;
+        const managersArr = managerArr(res)
+        inquirer.prompt({
+            name: "manager",
+            type: "list",
+            message: "Which manager's employees would you like to view?",
+            choices: managersArr
+        }).then(answer => {
+            // console.log(answer.manager.id)
+            connection.query(`
+               SELECT e.id, e.first_name, e.last_name
+                FROM employee e
+                JOIN employee m
+                ON e.manager_id = m.id
+                WHERE m.id=?;
+            `, [answer.manager.id], (err, res) => {
+                // console.log(res);
+                consoleResult(err, res)
+            })
+        }).catch((error) => {
+            console.log(error.message);
+            res.json({ error: error.message });
+        })
+    });
+}
+
+
+
+const addEmp = () => {
+    // const toPush = result.id + ". " + result.first_name + " " + result.last_name;
+    connection.query(`
+    SELECT DISTINCT m.id, m.first_name, m.last_name
+    FROM employee e
+    JOIN employee m
+    ON e.manager_id = m.id
+    JOIN role r
+    ON m.role_id = r.id
+    `, (err, res) => {
+        if (err) throw err;
+        const managersArr = managerArr(res);
+        managersArr.push({ name: "0. None" });
+
+        connection.query(`SELECT * FROM role`, (err, res) => {
+            if (err) throw err;
+            const rolesArr = roleArr(res);
+            inquirer.prompt([
+                {
+                    name: "first",
+                    type: "input",
+                    message: "Enter employee's first name:",
+                    validate: validator
+                },
+                {
+                    name: "last",
+                    type: "input",
+                    message: "Enter employee's last name:",
+                    validate: validator
+                },
+                {
+                    name: "role",
+                    type: "list",
+                    message: "What is their role in the company?",
+                    choices: rolesArr
+                },
+                {
+                    name: "manager",
+                    type: "list",
+                    message: "Who manages new employee?",
+                    choices: managersArr
+                },
+            ]).then((answer) => {
+                // console.log(answer.manager)
+                const newEmployee = {
+                    first_name: answer.first,
+                    last_name: answer.last,
+                    role_id: answer.role.id,
+                    manager_id: answer.manager.id
+                }
+
+                // console.table(newEmployee)
+                // if (role === "")
+                connection.query(`INSERT INTO employee SET ? `, newEmployee, (err, res) => {
+                    if (err) throw err;
+                    // console.log(res)
+                    allEmployees();
+                })
+            }).catch((error) => {
+                console.log(error.message);
+                res.json({ error: error.message });
+            })
+        })
+    })
+}
+
