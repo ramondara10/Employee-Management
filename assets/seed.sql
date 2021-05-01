785cd0304c08976d449aca345acdda82d25a8c73
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-05-01 14:04:25 -0500

    Update seed.sql
    adding employees

--- assets/seed.sql
+++ assets/seed.sql
@@ -1,8 +1,24 @@
 --employee section
 insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
+insert into employee(first_name, last_name, role_id, manager_id)values()
 
 --role section
 insert into role(title, salary, department_id)values()
+insert into role(title, salary, department_id)values()
+insert into role(title, salary, department_id)values()
+insert into role(title, salary, department_id)values()
+insert into role(title, salary, department_id)values()
+insert into role(title, salary, department_id)values()
+insert into role(title, salary, department_id)values()
 
 --department section 
+insert into department (name) values()
+insert into department (name) values()
 insert into department (name) values()
