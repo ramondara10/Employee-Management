424d934c8167b9b2a15d828d575206f6332431b8
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-04-30 19:55:05 -0500

    created mysql files
    created the seed and schema

---
+++ assets/seed.sql
@@ -0,0 +1,8 @@
+--employee section
+insert into employee(first_name, last_name, role_id, manager_id)values()
+
+--role section
+insert into role(title, salary, department_id)values()
+
+--department section 
+insert into department (name) values()
